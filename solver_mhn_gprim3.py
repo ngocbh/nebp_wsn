d9048446df2adb7a22c4f40588fdc03050a7f592
ngocjr7 <ngocjr7@gmail.com>
2020-10-16 19:54:35 +0700

    update gprim3 testing

--- solver_mhn_gprim3.py
+++ solver_mhn_gprim3.py
@@ -184,6 +184,6 @@ if __name__ == '__main__':
     config = {'data': {'max_hop': 10},
                   'models': {'gens': 200},
 		  'encoding': {'init_method': 'PrimRST'}}
-    solve('data/_medium/multi_hop/medium_ga-dem1_r25_1_40.json', model = '1.8.8.0', 
+    solve('data/_medium/multi_hop/medium_ga-dem1_r25_1_40.json', model = '1.8.8.0.1', 
           config=config)
 
